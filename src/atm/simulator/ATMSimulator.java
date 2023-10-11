1bf939ce9e8842f89c01d61be5264c9c2b91bdc2
gamzegenc99 <gamzegenc99@gmail.com>
2023-10-11 15:12:20 +0300

    ATM simulator

---
+++ src/atm/simulator/ATMSimulator.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atm.simulator;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author gamze
+ */
+public class ATMSimulator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String sifre = "3401";
+         
+        while(true){
+                System.out.println("Şifrenizi giriniz..");
+                sifre = scanner.nextLine();// nextLine()  kullanıcının girdiği tam satırı (boşluklar dahil) alır,next() ise yalnızca ilk kelimeyi alır.
+
+                if(sifre.equals("3401")){
+                     System.out.println("Şifreniz doğru giriş yapılıyor..."); break;
+                } 
+                else if (!sifre.equals("3401")){
+
+                    System.out.println("şifre hatalı tekrar deneyin");
+                }      
+        }
+       
+        double bakiye = 2000;
+        String islemler =   "1.işlem: Bakiye öğrenme \n"  +
+                            "2.işlem: Para Çekme \n" +
+                            "3.işlem: Para Yatırma \n" +
+                            "çıkış için q ya basınız";
+        System.out.println("**************************************");
+        System.out.println("İşlemler\n"  +  islemler);
+        System.out.println("**************************************");
+
+        OUTER: //label: döngünün dışındaki  kontrol noktası,case q durumunda döngüden yani tamamen switch-case yapısından çıkmayı sağlar.
+        while (true) {
+            System.out.println(" İşlem seçiniz ");
+            String islem = scanner.nextLine();
+            switch (islem) { //Neden switch case yapısını kullandım?Çünkü if else ile komplex oluyor ve hata verebiliyor bazı koşulları sunamıyordum.
+                case "q":
+                        System.out.println("Programdan çıkılıyor");
+                        break OUTER;
+                case "1":
+                        System.out.println(" Bakiyeniz : " +  bakiye);
+                        break;
+                case "2":
+
+                        System.out.println("Çekmek istediğiniz tutarı girin");
+                        int tutar = scanner.nextInt();
+                        scanner.nextLine();
+                        if (bakiye - tutar < 0) {
+
+                            System.out.println("Yeterli Bakiye Yok . Bakiyeniz : " + bakiye);
+                        }
+                        else {
+                            bakiye -= tutar;
+                            System.out.println("Yeni bakiyeniz : " + bakiye); //güncel bakiyeyi de kullanıcıya gösterelim.
+                        }      break;
+
+                case "3":
+
+                        System.out.print("Yatırmak istediğiniz tutar : ");
+                        int miktar = scanner.nextInt();
+                        scanner.nextLine();
+                        bakiye += miktar;
+                        System.out.println("Yeni Bakiyeniz  : " + bakiye);
+                        break;
+
+                default:// üstteki durumlar uymazsa default  çalışır.
+                        System.out.println("Geçersiz İşlem"); 
+                        break;
+            }  
+        }  
+    }
+}
+
